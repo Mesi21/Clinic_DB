dade51de6eca977afef8a47e62037f3308688e69
Mesi <mse212002@gmail.com>
2022-09-13 09:37:44 +0300

    :card_file_box: add data to owners table

--- data.sql
+++ data.sql
@@ -20,3 +20,11 @@ VALUES ('CHARMANDER', '2021-02-08',0, False, -11),
        ('BLOSSOM', '1998-10-13', 3, True, 17),
        ('DITTO', '2022-05-14', 4, True, 22)
 ;
+
+INSERT INTO owners(full_name, age)
+    VALUES ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
